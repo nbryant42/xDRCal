37d911ae2056df8150b9d6e18fb09dd1d59334a1
Nathan Bryant <nbryant@optonline.net>
2025-07-20 18:11:40 -0400

    need an await here

--- xDRCal/Controls/CalibrationDisplay.cs
+++ xDRCal/Controls/CalibrationDisplay.cs
@@ -264,11 +264,8 @@ public sealed partial class CalibrationDisplay : Panel, IDisposable, ISurfaceHos
 
             _dwriteFactory = DWrite.DWriteCreateFactory<IDWriteFactory>();
 
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-            // fire-and-forget is fine here; no RenderLoop for these.
-            _leftBtnSurface.ResizeRenderTarget(false);
-            _rightBtnSurface.ResizeRenderTarget(false);
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            await _leftBtnSurface.ResizeRenderTarget(false);
+            await _rightBtnSurface.ResizeRenderTarget(false);
             await _testPatternSurface.ResizeRenderTarget(true);
             StartRenderLoop();
         }
